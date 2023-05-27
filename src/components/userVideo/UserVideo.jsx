b461ca91643157331ca1a7240c331031c8fd0d3a
tanu <tanu.kumari@scorpius.com.my>
2023-05-27 23:39:23 +0530

    explore ui completed

---
+++ src/components/userVideo/UserVideo.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+const UserVideo = () => {
+    return (
+        <div style={{ maxWidth: "550px", margin: "0px auto" }}>
+            <div style={{ display: "flex", justifyContent: "space-around", margin: "55px 0px", borderBottom: "0px solid grey" }}>
+                <div>
+                    <img style={{ width: "100px", height: "100px", borderRadius: "80px" }} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKi12A3tgAu62QdbNgTljOG2PoS49BLjHOtg&usqp=CAU" alt="" />
+                </div>
+                <div>
+                    <h4>Tanu Kumari</h4>
+                    <div style={{ display: "flex", justifyContent: "space-between", width: "108%" }}>
+                        <h6>40 posts</h6>
+                        <h6>180 followers</h6>
+                        <h6>140 following</h6>
+                    </div>
+                </div>
+            </div>
+            <div className="gallery">
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+                <video width="180" height="100" controls autoplay muted>
+                    <source src="https://player.vimeo.com/external/377769527.sd.mp4?s=d2fbb91c4b725c7f3cbd506c0b5bbc8c5af30fee&profile_id=164&oauth2_token_id=57447761" alt="" />
+                </video>
+
+
+            </div>
+
+
+        </div >
+    )
+}
+export default UserVideo;
